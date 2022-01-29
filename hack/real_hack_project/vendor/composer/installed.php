85c19ebd26f1b5c5fb0e527979b3aab6ba135e2a
Akash Kumar <akashkumar88@gmail.com>
2022-01-29 14:04:45 +0000

    re-indexed

---
+++ hack/real_hack_project/vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a0d891490741042eea2ed72d218a9139fc6c32c1',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a0d891490741042eea2ed72d218a9139fc6c32c1',
+            'dev_requirement' => false,
+        ),
+        'facebook/hhvm-autoload' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.*',
+            ),
+        ),
+        'hhvm/hhvm-autoload' => array(
+            'pretty_version' => 'v3.3.1',
+            'version' => '3.3.1.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../hhvm/hhvm-autoload',
+            'aliases' => array(),
+            'reference' => '4dd560e1a161597ea80aeb9a0e644bf79426a22c',
+            'dev_requirement' => false,
+        ),
+        'hhvm/hsl' => array(
+            'pretty_version' => 'v4.108.1',
+            'version' => '4.108.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../hhvm/hsl',
+            'aliases' => array(),
+            'reference' => '3b4375e6adf63ac9171721b031b662dd3524c5bb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
